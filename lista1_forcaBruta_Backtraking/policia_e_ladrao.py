0b0fa986615311e5c971250f880f9f7c177028fa
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2018-06-08 15:33:14 -0300

    adiciona map ao invés da "matriz na mão"

--- lista1_forcaBruta_Backtraking/policia_e_ladrao.py
+++ lista1_forcaBruta_Backtraking/policia_e_ladrao.py
@@ -1,4 +1,4 @@
-entrada = int(raw_input())
+n = int(raw_input())
 cont = 0
 
 def caminha(m, l, c):
@@ -10,34 +10,34 @@ def caminha(m, l, c):
 	if l == 4 and c == 4:
 		return
 	
-	if c + 1 < 5 and (m[l][c+1]) == '0':
+	if c + 1 < 5 and (m[l][c+1]) == 0:
 		caminha(m, l, c+1)
-	if c - 1 >= 0 and (m[l][c-1]) == '0':
+	if c - 1 >= 0 and (m[l][c-1]) == 0:
 		caminha(m, l, c-1)
 	
-	if l + 1 < 5 and (m[l+1][c]) == '0':
+	if l + 1 < 5 and (m[l+1][c]) == 0:
 		caminha(m, l+1, c)
-	if l - 1 >= 0 and (m[l-1][c]) == '0':
+	if l - 1 >= 0 and (m[l-1][c]) == 0:
 		caminha(m, l-1, c)
 	
-def geraMatriz():
-	matriz = []
-	for m in range(5):
-		linha = raw_input().split()
-		matriz.append(linha)
-
-	return matriz
+while cont < n: 
+	tabuleiro = []
+	entrada = map(int,raw_input().split())
 	
+	if (len(entrada)==0):
+		c = 5
+	else:
+		c = 4
+		tabuleiro.append(entrada)
 	
-while cont < entrada:	
-	tabuleiro = geraMatriz()
+	for i in range(c):
+		aux = map(int,raw_input().split())
+		tabuleiro.append(aux)
+		
+	caminha(tabuleiro,0,0)
 	
-	caminha(tabuleiro, 0, 0)
-	
-	if tabuleiro[4][4] == -1:
+	if(tabuleiro[4][4] == -1):
 		print "COPS"
 	else:
 		print "ROBBERS"
-
 	cont += 1
-	print
