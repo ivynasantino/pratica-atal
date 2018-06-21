7b567e578dba3219508b98341c969b5f52a48a3f
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2018-06-20 21:19:14 -0300

    runtime

--- lista2_guloso_divisaoeconquista/1025.py
+++ lista2_guloso_divisaoeconquista/1025.py
@@ -1,5 +1,3 @@
-inp = map(int, raw_input().split())
-
 def buscaBinaria(lista, valor, ini, fim):
 	if valor not in lista:
 		return -1
@@ -12,22 +10,46 @@ def buscaBinaria(lista, valor, ini, fim):
 		return buscaBinaria(lista, valor, ini, meio) 
 
 
-for case in range(inp[1]):	
-	if inp[1] == 0: break	
+def primeiroMarmore(result, lista):
+	primeiroIndice = result-1
+	for i in range(len(lista)-1,-1,-1):
+		if lista[result] == lista:
+			primeiroIndice -= 1
+			
+	return primeiroIndice
+
+
+case = 1
+
+while True:	
+	inp = map(int, raw_input().split())
+	if inp[1] == 0 and inp[0] == 0: break	
 	marmores = []
 
 	for i in range(inp[0]):
 		entrada = int(raw_input())
 		marmores.append(entrada)
 	
-	marmore = int(raw_input())
+	marmoreChute = int(raw_input())
 
 	marmores.sort()
 
-	print "CASE# %d" % (case+1)
-
-	resultado = buscaBinaria(marmores, marmore, 0, len(marmores)-1)
+	resultado = buscaBinaria(marmores, marmoreChute, 0, len(marmores)-1)
 	if resultado == -1:
-		print "%d not found" % marmore
+		print "CASE# %d" % (case)
+		print "%d not found" % marmoreChute
+		marmoreChute = int(raw_input())
+		resultado = buscaBinaria(marmores, marmoreChute, 0, len(marmores)-1)
+		chance = inp[1]-1
+		while chance != 0:
+			if marmoreChute == marmores[resultado]:
+				print "%d found at %d" % (marmoreChute, resultado+1)
+			chance -= 1
+		
 	else:
-		print "%d found at %d" % (marmore, resultado+1)		
+		print "CASE# %d" % (case)
+		result = primeiroMarmore(resultado, marmores)
+		print "%d found at %d" % (marmoreChute, result+2)	
+	
+	case += 1	
+
