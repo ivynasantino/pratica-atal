cb6e0374f0148048bb7de98244116496d113b7cb
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2018-07-02 19:59:41 -0300

    adiciona estrutura de PD

--- lista3_PD/1029.py
+++ lista3_PD/1029.py
@@ -1,16 +1,20 @@
 n = int(raw_input())
 cont = -1
 
-def fib(num):
+seq_f = []
+calls = []
+
+seq_f.append(0)
+seq_f.append(1)
+
+print seq_f
+def fib(n):
 	global cont
-	cont += 1
-	if num == 0:
-		return 0
-	elif num == 1:
-		return 1
-	else:
-		return fib(num-1) + fib(num-2)
-		
+	for i in xrange(2, n):
+		seq_f[i] = seq_f[i-1] + seq_f[i-2]
+		cont += 1
+	
+	
 for i in xrange(n):
 	f = int(raw_input())
 	calc = fib(f)
