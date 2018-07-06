064bf27228455985a4253168dbf49f68d60fdc01
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2018-07-06 17:27:35 -0300

    Passou

--- lista3_PD/1029.py
+++ lista3_PD/1029.py
@@ -1,22 +1,18 @@
 n = int(raw_input())
-cont = -1
 
-seq_f = []
-calls = []
-
-seq_f.append(0)
-seq_f.append(1)
-
-print seq_f
-def fib(n):
-	global cont
-	for i in xrange(2, n):
-		seq_f[i] = seq_f[i-1] + seq_f[i-2]
-		cont += 1
+def fib(n):	
+	seq_f = [0,1]
+	calls = [0,0]
+	
+	for i in xrange(2, n+1):
+		seq_f.append(seq_f[i-1] + seq_f[i-2])
+		calls.append(calls[i-1] + calls[i-2] + 2)
+	
+	return seq_f[n], calls[n]
 	
 	
 for i in xrange(n):
 	f = int(raw_input())
-	calc = fib(f)
-	print "fib(%d) = %d calls = %d" % (f, cont, calc)
-	cont = -1 
+	seq, call = fib(f)
+	print "fib(%d) = %d calls = %d" % (f, call, seq)
+	
